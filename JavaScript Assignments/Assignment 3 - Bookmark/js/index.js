35cd9304aa15809aeeea7271d3bd4bd8d2742c1e
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-03-26 01:31:47 +0200

    Adding,deleting,visiting bookmark functions are done

--- JavaScript Assignments/Assignment 3 - Bookmark/js/index.js
+++ JavaScript Assignments/Assignment 3 - Bookmark/js/index.js
@@ -20,25 +20,61 @@ function addBookmark() {
 
     var urlRegex = /^https:\/\/([\w\-]+\.)+[\w\-]{2,}/i;
 
+    // if (name === '' || url === '') {
+    //     alert('Please fill in both fields.');
+    //     return;
+    // }
+
     if (name === '' || url === '') {
-        alert('Please fill in both fields.');
+        Swal.fire({
+            icon: 'warning',
+            title: 'Empty Fields',
+            text: 'Please fill in both site name and URL.',
+            confirmButtonColor: 'rgb(160, 0, 0)'
+        });
         return;
     }
+    
 
     var isDuplicate = bookmarksContainer.some(function (bookmark) {
         return bookmark.name.toLowerCase() === name.toLowerCase();
     });
 
+    // if (isDuplicate) {
+    //     alert('This website name is already added.');
+    //     return;
+    // }
+
     if (isDuplicate) {
-        alert('This website name is already added.');
+        Swal.fire({
+            icon: 'error',
+            title: 'Duplicate Name',
+            text: 'This website name is already added.',
+            confirmButtonColor: 'rgb(160, 0, 0)'
+        });
         return;
-    }
+    }    
+
+    // if (!urlRegex.test(url)) {
+    //     alert('Invalid URL! URL must start with "https://" and be in a proper format.');
+    //     return;
+    // }
 
     if (!urlRegex.test(url)) {
-        alert('Invalid URL! URL must start with "https://" and be in a proper format.');
+        Swal.fire({
+            icon: 'error',
+            title: 'Invalid URL',
+            html: `<b>Site Name or URL is not valid.</b><br><br>
+                    Please follow the rules below:<br><br>
+                    <ul style="text-align: left; list-style: none;">
+                        <li><i class="fa-regular fa-circle-right p-2" style="color:rgb(160, 0, 0);"></i> Site name must contain at least 3 characters</li>
+                        <li><i class="fa-regular fa-circle-right p-2" style="color:rgb(160, 0, 0);"></i> Site URL must start with <strong>https://</strong> and be valid</li>
+                    </ul>`,
+                    confirmButtonColor: 'rgb(160, 0, 0)'
+                });
         return;
     }
-
+    
     var bookmark = {
         name: name,
         url: url
@@ -47,6 +83,12 @@ function addBookmark() {
     bookmarksContainer.push(bookmark);
     localStorage.setItem('bookmark', JSON.stringify(bookmarksContainer));
     console.log(bookmarksContainer);
+    Swal.fire({
+        icon: 'success',
+        title: 'Bookmark Added!',
+        showConfirmButton: false,
+        timer: 1200
+    });    
     displayBookmark(bookmarksContainer);
     clearForm();
 }
@@ -55,20 +97,20 @@ function addBookmark() {
 
 function displayBookmark(arr){
     var bookmarkStorage = ``;
-    for( let websiteIndex = 1; websiteIndex<arr.length; websiteIndex++){
+    for( let websiteIndex = 0; websiteIndex<arr.length; websiteIndex++){
         bookmarkStorage += 
                     `
                     <tr>
-                        <td class="pt-3">${websiteIndex}</td>
+                        <td class="pt-3">${websiteIndex+1}</td>
                         <td class="pt-3">${arr[websiteIndex].name}</td>
                         <td>
-                            <button id="visitBtn" class="btn-visit btn-white border-0 px-3 py-2 rounded-3">
+                            <button onclick="visitBookmark('${arr[websiteIndex].url}')" id="visitBtn" class="btn-visit btn-white border-0 px-3 py-2 rounded-3">
                                 <i class="fa-solid fa-eye"></i>
                                 Visit
                             </button>
                         </td>
                         <td>
-                            <button id="deleteBtn" class="btn-white bg-danger border-0 px-3 py-2 rounded-3">
+                            <button onclick="deleteBookmark(${websiteIndex})" id="deleteBtn" class="btn-white btn-delete bg-danger border-0 px-3 py-2 rounded-3">
                                 <i class="fa-solid fa-trash-can"></i>
                                 Delete
                             </button>
@@ -84,4 +126,19 @@ function displayBookmark(arr){
 function clearForm(){
     siteName.value = null;
     siteURL.value = null;
+}
+
+/* ====================================== DELETE BOOKMARK ====================================== */
+
+function deleteBookmark(index){
+    bookmarksContainer.splice(index,1);
+    localStorage.setItem('bookmark', JSON.stringify(bookmarksContainer));
+    console.log(bookmarksContainer);
+    displayBookmark(bookmarksContainer);
+}
+
+/* ====================================== VISIT BOOKMARK ====================================== */
+
+function visitBookmark(url){
+    window.open(url, '_blank');
 }
