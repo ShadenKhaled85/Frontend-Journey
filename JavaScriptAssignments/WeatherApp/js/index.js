e481deae557f61c2171b3c4aa3839318dae9b893
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-05-10 15:17:45 +0300

    Search function

--- JavaScriptAssignments/WeatherApp/js/index.js
+++ JavaScriptAssignments/WeatherApp/js/index.js
@@ -33,10 +33,9 @@ let searchInput = document.querySelector('#search');
 
 /* ================================= FETCH API DATA =================================  */
 
-async function getWeatherData(searchQuery){
-    // let weatherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3b69cc78408a4c8fb18124357250505&q=${searchQuery}&days=3`);
-
-    let weatherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3b69cc78408a4c8fb18124357250505&q=London&days=3`);
+async function getWeatherData(citySearchQuery){
+    // let weatherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3b69cc78408a4c8fb18124357250505&q=London&days=3`);
+    let weatherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3b69cc78408a4c8fb18124357250505&q=${citySearchQuery}&days=3`);
     // console.log(weatherResponse);
     let weatherData = await weatherResponse.json();
     // console.log(weatherData);
@@ -78,15 +77,16 @@ function displayNextData(data){
 
 /* ================================= SEARCH =================================  */
 
-// searchInput.addEventListener('keyup',function(e){
-//     console.log(searchInput.value); // value of entered keys
-// })
+searchInput.addEventListener('input',function(e){
+    // console.log(searchInput.value); // value of entered keys
+    startApp(searchInput.value);
+})
 
 /* ================================= START APP =================================  */
 
 // START APP to call all functions
-async function startApp(){
-    let weatherData = await getWeatherData();
+async function startApp(city = 'cairo'){ // default value
+    let weatherData = await getWeatherData(city);
     // console.log(weatherData);
     displayTodayData(weatherData);
     displayNextData(weatherData);
@@ -184,25 +184,3 @@ startApp();
 // search("cairo");
 
 
-// async function search(a){let t=await
-//     fetch(`https://api.weatherapi.com/v1/forecast.json?key=7d77b96c972b4d119a3151101212704&q=${a}&days=3`);if(t.ok&&400!=t.status){let
-//     a=await
-// t.json();displayCurrent(a.location,a.current),displayAnother(a.forecast.forecastday)}}document.getElementById("search").addEventListener("keyup",a=>{search(a.target.value)});var
-//     days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];const
-//     monthNames=["January","February","March","April","May","June","July","August","September","October","November","December"];function
-//     displayCurrent(a,t){if(null!=t){var e=new Date(t.last_updated.replace(" ","T"));let n=`<div class="today forecast">
-//         \n <div class="forecast-header" id="today">\n <div class="day">${days[e.getDay()]}</div>\n <div class=" date">
-//                 ${e.getDate()+monthNames[e.getMonth()]}</div>\n </div> \x3c!-- .forecast-header --\x3e\n <div
-//             class="forecast-content" id="current">\n <div class="location">${a.name}</div>\n <div class="degree">\n <div
-//                     class="num">${t.temp_c}<sup>o</sup>C</div>\n \n <div class="forecast-icon">\n <img
-//                         src="https:${t.condition.icon}" alt="" width=90>\n </div>\t\n \n </div>\n <div class="custom">
-//                 ${t.condition.text}</div>\n <span><img src="images/icon-umberella.png"
-//                     alt="">20%</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-wind.png"
-//                     alt="">18km/h</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-compass.png" alt="">East</span>\n
-//         </div>\n</div>`;document.getElementById("forecast").innerHTML=n}}function displayAnother(a){let t="";for(let
-//     e=1;e<a.length;e++)t+=`\t<div class="forecast">\n <div class="forecast-header">\n <div class="day">${days[new
-//                 Date(a[e].date.replace(" ","T")).getDay()]}</div>\n </div> \x3c!-- .forecast-header --\x3e\n <div
-//             class="forecast-content">\n <div class="forecast-icon">\n <img src="https:${a[e].day.condition.icon}" alt=""
-//                     width=48>\n </div>\n <div class="degree">${a[e].day.maxtemp_c}<sup>o</sup>C</div>\n
-//             <small>${a[e].day.mintemp_c}<sup>o</sup></small>\n <div class="custom">${a[e].day.condition.text}</div>\n
-//         </div>\n </div>`;document.getElementById("forecast").innerHTML+=t}search("cairo");
